f54dc0cb25656d8d1b922abb46e9447d7143bd25
Joachim Klug <code@joachimklug.com>
2023-08-06 16:54:16 +0200

    feat: 🎸 Prevent printing when printer is not ready

--- components/fileDetails/FileActions.tsx
+++ components/fileDetails/FileActions.tsx
@@ -1,21 +1,30 @@
 import { View } from "@/components/Themed";
 import { File } from "@/models/Files";
 import { startPrint } from "@/services/files";
+import { fetchPrinter } from "@/services/printer";
 import { router } from "expo-router";
 import { StyleSheet } from "react-native";
 import { Button } from "react-native-paper";
+import { useQuery } from "react-query";
 
 interface Props {
   file: File;
 }
 
 export default function FileActions({ file }: Props) {
+  const { data: printer } = useQuery("fetchPrinter", fetchPrinter, { enabled: false });
+
   return (
     <View style={styles.container}>
       <Button icon="close" mode="outlined" onPress={() => router.back()}>
         Close
       </Button>
-      <Button icon="printer-3d-nozzle-outline" mode="contained" onPress={() => startPrint(file.origin, file.path)}>
+      <Button
+        icon="printer-3d-nozzle-outline"
+        mode="contained"
+        onPress={() => startPrint(file.origin, file.path)}
+        disabled={!printer?.state.flags.ready}
+      >
         Print
       </Button>
     </View>
